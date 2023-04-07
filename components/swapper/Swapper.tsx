5b2bf6c6f2148644b8a2e099d7d9fc40c04d6ce4
xapski <xapski2671@gmail.com>
2023-04-07 10:39:58 +0000

    Apr 7, 2023, 11:39 AM

--- components/swapper/Swapper.tsx
+++ components/swapper/Swapper.tsx
@@ -2,8 +2,31 @@ export default function Swapper() {
   return (
     <div className="sw">
       <div className="sw-menu">
-        <div className="sw-menu-swap"></div>
-        <div className="sw-menu-settings"></div>
+        <div className="sw-menu-swap">{"Swap"}</div>
+        <div className="sw-menu-settings">
+          {/* fontawesomeicon */}
+        </div>
+      </div>
+      <div className="sw-swap-box">
+        <div className="sw-input-cont-wrapper">
+          <div className="sw-input-box">
+            <input type="text" className="sw-inpt"/>
+            <div className="sw-token-select">
+              {/* faethicon */}
+              <span>{"ETH"}</span>
+              {/* fadropicon */}
+            </div>
+          </div>
+          <div className="sw-input-box">
+            <input type="text" className="sw-inpt"/>
+            <div className="sw-token-select">
+              {/* faethicon */}
+              <span>{"ETH"}</span>
+              {/* fadropicon */}
+            </div>
+          </div>
+        </div>
+        <button>{"Swap"}</button>
       </div>
       
     </div>
