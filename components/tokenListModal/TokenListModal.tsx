fe9e19d91711f36a2ff13f54d7b4c5a94226f5de
xapski <xapski2671@gmail.com>
2023-04-10 05:23:18 +0000

    Apr 10, 2023, 6:23 AM

--- components/tokenListModal/TokenListModal.tsx
+++ components/tokenListModal/TokenListModal.tsx
@@ -1,4 +1,6 @@
 import { gtkn } from "@/types"
+import { useState } from "react"
+import useInfiniteScroll from "react-infinite-scroll-hook"
 
 interface props {
   offMe:Function
@@ -8,6 +10,19 @@ interface props {
 }
 
 export default function TokenListModal({ offMe, tokens, type, selTkn }:props) {
+  const [loading, setLoading] = useState(false)
+  const [hasNext, setHasNext] = useState(true)
+  const [error, setError] = useState(false)
+  const [offset, setOffset] = useState(50)
+
+  const [sentryRef, { rootRef }] = useInfiniteScroll({
+    loading: loading,
+    hasNextPage: hasNext,
+    disabled: !!error,
+    onLoadMore: ()=>{offset >= 4060 ? setHasNext(false) : setOffset(prev=>prev + 50)},
+    rootMargin: "0px 0px 400px 0px"
+  })
+
   return (
     <div className="tlm-cont">
       <div className="tlm-reactive" onClick={()=>{offMe()}}>
@@ -15,8 +30,8 @@ export default function TokenListModal({ offMe, tokens, type, selTkn }:props) {
       <div className="tlm">
         <div className="tlm-box">
           <input type="text" className="tlm-search" placeholder="Search token name"/>
-          <div className="tlm-grp">
-            {tokens.slice(0,50).map((token, index)=>{
+          <div className="tlm-grp" ref={rootRef}>
+            {tokens.slice(0,offset).map((token, index)=>{
               return (
                 <div key={index} className="tlm-tkn-grp" onClick={()=>{selTkn(token); offMe()}}>
                   <img src={token.logoURI} alt="tkn_img" className="tlm-tkn-img"/>
@@ -25,6 +40,12 @@ export default function TokenListModal({ offMe, tokens, type, selTkn }:props) {
                 </div>
               )
             })}
+            {(loading || hasNext) && (
+              <div ref={sentryRef}>
+                {"Loading..."}
+              </div>
+            )
+            }
           </div>
         </div>
         <div className="tlm-edge">
