d3461b003d1ff862aebee743fa0af30c73c3508a
xapski <xapski2671@gmail.com>
2023-04-10 22:53:42 +0000

    Apr 10, 2023, 11:53 PM

--- components/swapper/Swapper.tsx
+++ components/swapper/Swapper.tsx
@@ -7,6 +7,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useContext, useState, useEffect } from "react"
 import { TokenListModal } from "../exportComps"
 import { ethers } from "ethers"
+import ERC20 from "@/constants/abis/ERC20.json"
 
 const ethTkn:gtkn = {
   chainId: 1,
@@ -29,6 +30,17 @@ export default function Swapper({ tokens }:{tokens:any[]}) {
   const [payUSDRate, setPayUSDRate] = useState("")
   const [recUSDRate, setRecUSDRate] = useState("")
   const [dloading, setDloading] = useState(false)
+  const [pBal, setPBal] = useState("")
+
+  async function findBalance(){
+    const erc20Ctrt = new ethers.Contract(payTkn.address, ERC20.abi, signer)
+    try {
+      const max = await erc20Ctrt.balanceOf(account)
+      setPBal(max.toString())
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   async function getQuote(value:string, type:string){
     setDloading(true)
@@ -47,17 +59,24 @@ export default function Swapper({ tokens }:{tokens:any[]}) {
         const response = await fetch(
           `https://api.0x.org/swap/v1/price?sellToken=${params.sellToken}&buyToken=${params.buyToken}&${type == "pay" ? "sellAmount=" + params.sellAmount : "buyAmount=" + params.buyAmount }&includedSources=${params.includedSources}`,
         ).then(res=>res.json()).then((data)=>{
+          // console.log(data)
           setPayUSDRate(Number((((Number(data.sellAmount) / (Math.pow(10,payTkn.decimals))) / Number(data.sellTokenToEthRate)) * Number(ethPrice)).toFixed(2)).toLocaleString())
           setRecUSDRate(Number((((Number(data.buyAmount) / (Math.pow(10,recTkn.decimals))) / Number(data.buyTokenToEthRate)) * Number(ethPrice)).toFixed(2)).toLocaleString())
           type == "pay" ? setRecVal((Number(data.buyAmount) / (Math.pow(10,recTkn.decimals))).toFixed(4)) : setPayVal((Number(data.sellAmount) / (Math.pow(10,payTkn.decimals))).toFixed(4))
+          setRprice((type == "pay" ? 1 / Number(data.price) : Number(data.price)).toFixed(4))
         })
-        console.log("Uniswap Quote",)
         setDloading(false)
       } catch (err) {
         console.error(err)
       }
     }
   }
+
+  useEffect(()=>{
+    if(isConnected && payTkn && payTkn.address){
+      findBalance()
+    }
+  },[isConnected, payTkn])
 
   return (
     <div className="sw">
@@ -80,12 +99,12 @@ export default function Swapper({ tokens }:{tokens:any[]}) {
           <div className="sw-inpt-grp">
             <div className="sw-py-max-grp">
               <span className="sw-py">{"PAY"}</span>
-              <span className="sw-max">{"MAX: 0.32 WETH"}</span>
+              <span className="sw-max">{`${payTkn && pBal ? "MAX: " + pBal + " " + payTkn.symbol : ""}`}</span>
             </div>
             <div className="sw-inpt-cont">
               <div className="sw-inpt-box">
                 <input type="number" className="sw-inpt" 
-                  onChange={(e)=>{setPayVal(e.target.value); !dloading && getQuote(e.target.value, "pay")}} 
+                  onChange={(e)=>{setPayVal(e.target.value); !dloading ? getQuote(e.target.value, "pay") : setTimeout(()=>{getQuote(payVal, "pay")},1000)}} 
                   value={payVal}
                 />
                 <div className="sw-tkn-sel">
@@ -107,12 +126,12 @@ export default function Swapper({ tokens }:{tokens:any[]}) {
           <div className="sw-inpt-grp">
             <div className="sw-py-max-grp">
               <span className="sw-py">{"RECEIVE"}</span>
-              <span className="sw-py">{"1 ETH = 3 UNI"}</span>
+              <span className="sw-py">{recTkn && recTkn.symbol && `${rprice && "1"} ${recTkn.symbol.substring(0,4)} = ${rprice} ${payTkn.symbol.substring(0,4)}`}</span>
             </div>
             <div className="sw-inpt-cont">
               <div className="sw-inpt-box">
                 <input type="number" className="sw-inpt" 
-                  onChange={(e)=>{setRecVal(e.target.value); !dloading && getQuote(e.target.value, "rec")}} 
+                  onChange={(e)=>{setRecVal(e.target.value); !dloading ? getQuote(e.target.value, "rec") : setTimeout(()=>{getQuote(recVal, "rec")},1000)}} 
                   value={recVal}
                 />
                 <div className="sw-tkn-sel">
